b50f4390d6050d7f03ccd49632b637a0046ced85
bcabrera1494 <b.cabrera1494@gmail.com>
2024-06-17 08:27:57 -0700

    Declare parameter str. Write logic to reverse str.
    Declare variable string with empty string.
    Declare variable stringArray to derive array from string.
    Declare variable reverseArray to reverse derived array.
    Return reverseArray.
    Call function in console log with string 'hello there'.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+let string = "";
+let stringArray = Array.from(string);
+let reverseArray = reverse(stringArray);
+return reverseArray;
 };
 
+console.log(reverseString('hello there'));
 // Do not edit below this line
 module.exports = reverseString;
