f9832387449ac722f13357889843bc1f92235c6b
bcabrera1494 <b.cabrera1494@gmail.com>
2024-06-16 18:42:54 -0700

    Enable challenges and add error message logic.
    Call function using arguments provided in challenges 1-6.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,11 +1,13 @@
-const repeatString = function() {
-let returnText = '';
-for (let r = 0; r < 3; r++){
-    returnText += 'hey';
-}
-return returnText
-}
-
+const repeatString = function(str,times) {
+    let result = ''
+    for (let r = 0; r < times && Math.sign(r) !== -1; r++){
+    result += str;
+    };
+    if ((Math.sign(times)) === -1){
+        let errorMessage = 'ERROR'
+        return errorMessage;
+    }
+    else return result;
 };
 
 // Do not edit below this line
