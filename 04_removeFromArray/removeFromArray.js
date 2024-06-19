14cc133e5ad6c7f01ece8c1e246a644702a056a0
bcabrera1494 <b.cabrera1494@gmail.com>
2024-06-19 12:14:02 -0700

    Declare parameters arr, remove; write splice logic
    Declare removeSingleValue variable

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, remove) {
+let removeSingleValue = arr.splice(remove);
+return removeSingleValue;
 };
+console.log(removeFromArray([1, 2, 3, 4], 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
