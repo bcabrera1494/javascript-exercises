ae8f0b0c34f0e432d639530d2ce2c6954f0b7189
bcabrera1494 <b.cabrera1494@gmail.com>
2024-06-19 11:48:42 -0700

    Call parameter string and write logic for reversal
    Derive an array from string using array.from.
    Write for loop logic to return array in reverse.
    Declare variable for reverse array string.
    Return reverse array string.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,8 +1,13 @@
-const reverseString = function(str) {
-let string = "";
+const reverseString = function(string) {
 let stringArray = Array.from(string);
-let reverseArray = reverse(stringArray);
-return reverseArray;
+console.log(stringArray);
+let reverseArray = [];
+    for (let i = stringArray.length -1; i >=0; i--){
+    reverseArray.push(stringArray[i]);
+    }
+console.log(reverseArray);
+let reverseArrayString = reverseArray.toString();
+return reverseArrayString;
 };
 
 console.log(reverseString('hello there'));
