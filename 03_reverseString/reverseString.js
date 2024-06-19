7291e3a9a3246594131905d58acccbffe1d89a75
bcabrera1494 <b.cabrera1494@gmail.com>
2024-06-19 11:58:07 -0700

    Console log remaining reverse string tests.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -9,7 +9,9 @@ console.log(reverseArray);
 let reverseArrayString = reverseArray.join("");
 return reverseArrayString;
 };
-
+console.log(reverseString('hello'));
 console.log(reverseString('hello there'));
+console.log(reverseString('123! abc! Hello, Odinite.'));
+console.log(reverseString(''));
 // Do not edit below this line
 module.exports = reverseString;
