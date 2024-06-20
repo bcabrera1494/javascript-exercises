fb8d20f370a523486a3e4ed5dd85b36491cd99f6
bcabrera1494 <b.cabrera1494@gmail.com>
2024-06-20 16:41:26 -0700

    Use filter() method to remove select elements.
    Write logic for filter() method to create new array
    that passes test provided by function.
    Test provided by function requires that the return
    values != removeValue or alsoRemove.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,9 +1,12 @@
 const removeFromArray = function(arr, removeValue, ...alsoRemove) {
-    const index = arr.indexOf(removeValue);
-    const remove = arr.splice(index, 1);
-    return arr
+    function removeValues(value){
+        return value !=  removeValue && value != alsoRemove;
+        }
+    const newArray = arr.filter(removeValues);
+    return newArray;
+    
 }
 console.log(removeFromArray([1,2,3,4], (3)));
-
+console.log(removeFromArray([1,2,3,4], 3, 2));
 // Do not edit below this line
 module.exports = removeFromArray;
