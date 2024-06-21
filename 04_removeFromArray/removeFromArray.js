ddcfc7f2a46a51a0cd62001150a8123b0be16a2e
bcabrera1494 <b.cabrera1494@gmail.com>
2024-06-20 17:37:17 -0700

    Write if...else...logic for non present values.
    Declare const to check if array includes removeValue.
    Declare const to check if array includes alsoRemove.
    Write logic to return original arr if arr does not include removeValue
    or alsoRemove value (i.e. removeValue and alsoRemove == false).
    Function now ignores non present values.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,13 +1,32 @@
 const removeFromArray = function(arr, removeValue, ...alsoRemove) {
     function removeValues(value){
         return value !=  removeValue && value != alsoRemove;
-        }
-    const newArray = arr.filter(removeValues);
-    return newArray;
-    
+        };
+
+    let checkRemoveValue = arr.includes(removeValue);
+    let checkAlsoRemove = arr.includes(alsoRemove);
+
+ if (checkRemoveValue === false) {
+     checkAlsoRemove;
+     if (checkAlsoRemove === false){
+            return arr;
+        }    
+        return arr
+    }
+ else {
+     const newArray = arr.filter(removeValues);
+     return newArray;
+    }     
 }
-console.log(removeFromArray([1,2,3,4], (3)));
+console.log(removeFromArray([1,2,3,4], 3));
 console.log(removeFromArray([1,2,3,4], 3, 2));
 console.log(removeFromArray([1,2,2,3], 2));
+//Why doesn't this last console.log pass through lines 1-20 of the function? 
+console.log(removeFromArray([1,2,3,4], 7, "tacos"));
+/*
+console.log(removeFromArray[1,2,3,4], 7, 2);
+console.log(removeFromArray([1,2,3,4], 1, 2, 3, 4));
+console.log(removeFromArray(["hey", 2, 3, "ho"], 2, "ho"));
+console.log(removeFromArray([1,2,3]), "1", 3);*/
 // Do not edit below this line
 module.exports = removeFromArray;
