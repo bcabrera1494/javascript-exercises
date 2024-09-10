fd1d7ea9c995970c1f93d8339c6a14f0399e29fc
bcabrera1494 <b.cabrera1494@gmail.com>
2024-09-09 19:02:45 -0700

    Rework function to edit arr (original)
    Initial soultion returned new arrays and were difficult to access and
    return throughout the function. Therefore, trying to create a new soultion
    that edits the original array using the toSpliced method. New solution will
    first remove removeValue, then iterate through alsoRemove[] elements to check
    if they are present in arr. Current issue is that the function is currently
    returning "undefined" rather than the array splicedArray.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,33 +1,48 @@
 const removeFromArray = function(arr, removeValue, ...alsoRemove) {
-    function removeValues(value){
-        return value !=  removeValue && value != alsoRemove;
+    
+    //function that checks if removeValue is in arr, finds its index, splices it out, returns the edited array
+    checkAndRemove(removeValue);
+
+    function checkAndRemove(value){
+        //checks if removeValue matches any elements of arr
+        let checkInclusion = arr.includes(value);
+        
+        //finds the index of removeValue
+        let index = arr.indexOf(value);
+
+        //splices removeValue out of arr
+        const splicedArray = arr.toSpliced(index, 1);
+        
+        if (checkInclusion === true) { 
+        return splicedArray; //why does this still return undefined even if the variable is defined
         };
+    };
 
-    let checkRemoveValue = arr.includes(removeValue);
+};
 
- if (checkRemoveValue === false) {    
-        return arr}
- else {
-     const newArray = arr.filter(removeValues);
-     return newArray;
-    }     
+   /* function compareValues(){
+        for (const value of alsoRemove) {
+            //check if the current value is included in the newArray
+            let checkValue = newArray.includes(value);
+            //conditional statement that filters the current value OUT of newArray to return and updated array.
+            //aka arrayEdit
+            if (checkValue === true) {
+                let arrayEdit = newArray.filter(value);
+                return arrayEdit}
+            };
+            //Now the loop should restart and iterate over the next iterable and repeat until all iterables have
+            //been iterated through the loop.
+    };
+    compareValues(alsoRemove);*/
 
-   
-    function returnArrayFromAlsoRemove(arrValue){
-        return arrValue
-    } 
 
-    let alsoRemoveLoop = alsoRemove.includes(arrValue);
-}
 console.log(removeFromArray([1,2,3,4], 3));
 console.log(removeFromArray([1,2,3,4], 3, 2));
 console.log(removeFromArray([1,2,2,3], 2));
-//Why doesn't this last console.log pass through lines 1-20 of the function? 
 console.log(removeFromArray([1,2,3,4], 7, "tacos"));
-/*
 console.log(removeFromArray[1,2,3,4], 7, 2);
 console.log(removeFromArray([1,2,3,4], 1, 2, 3, 4));
 console.log(removeFromArray(["hey", 2, 3, "ho"], 2, "ho"));
-console.log(removeFromArray([1,2,3]), "1", 3);*/
+console.log(removeFromArray([1,2,3]), "1", 3);
 // Do not edit below this line
 module.exports = removeFromArray;
