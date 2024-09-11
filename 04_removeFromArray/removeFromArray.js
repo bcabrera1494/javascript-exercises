6e7213982a6550830611ee5250347dcde24d5b9a
bcabrera1494 <b.cabrera1494@gmail.com>
2024-09-11 16:06:03 -0700

    Remove redundant code, use filter method
    Remove redundant code that manually reads through every value and index
    of both arrays - too cluttered and inefficient in solving the problem of
    removing the values.
    
    Remove comments describing parameters to clear workspace.
    
    Commented out for...of loop due to ongoing issue with return value of filterArray
    being undefined.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,75 +1,26 @@
-const removeFromArray = function(arr, ...removeValues) { //changed the parameter so that ALL values are included in the array of rest parameters
-    //Create a function that returns the index of an element value ***maybe this is unnecessary?
-    //Parameters to pass through: array, index
-    /*function indexOfValue (array, element){
-        let index = array.indexOf(element);
-        return index;
-    }*/
+const removeFromArray = function(arr, ...removeValues) {
+    let editArray = [arr];
+    let removeArray = [...removeValues];
 
-    //Create a function that returns the value of an element, given its index
-    //Paremeters to pass through: array, index
-    function valueOfElement (array, index){
-        let element = array[index];
-        return element; //result should return the value (a number)
+    //Create a function that filters elements of removeValues that return "false" from checkAndRemoveValues
+    function filterArray (array){
+        let newArray = array.filter(checkValues);
+         //result should return edited array without the "false" elements
+        return newArray;
     }
-    let arrOne = valueOfElement (arr, 0);
-    let arrTwo = valueOfElement (arr, 1);
-    let arrThree = valueOfElement (arr, 2);
-    let arrFour = valueOfElement (arr, 3);
 
     //Create a function that checks if an element value is included in an array
-    //Parameters to pass through: array, element
-    function checkRemoveValues (array, element){
+    function checkValues (array, element){
         let check = array.includes(element);
-        return check; //result will be true or false
-    }
-    checkRemoveValues (removeValues, arrOne);
-        if (checkRemoveValues === true) {
-            spliceArray()
-        }
-    checkRemoveValues (removeValues, arrTwo);
-    checkRemoveValues (removeValues, arrThree);
-    checkRemoveValues (removeValues, arrFour);
-
-    //Create a function that splices an element from the array and returns the edited array 
-    function spliceArray (array,index){
-        let splice = array.toSpliced(index,1);
-        splice;
-        return array; //result should return edited array
-    }
-    //Create a loop that runs valueOfElement and checkRemoveValues on each element in the array
-    for (element of removeValues) {
-        valueOfElement(arr, 0);
-        checkRemoveValues();
-        //Write conditional statement that runs spliceArray if the checkRemoveValues is true 
-        if (checkRemoveValues === true) {
-            spliceArray(arr,0);
-            arr;
-        }
-        valueOfElement (arr, 1);
-        checkRemoveValues();
-        if (checkRemoveValues === true) {
-            spliceArray(arr, 1);
-            arr;
-        }
-        valueOfElement (arr, 2);
-        checkRemoveValues();
-        if (checkRemoveValues === true) {
-            spliceArray(arr, 2);
-            arr;
-        }
-        valueOfElement (arr, 3);
-        checkRemoveValues();
-        if (checkRemoveValues === true) {
-            spliceArray(arr, 3);
-            return arr;
-        }
-        else 
-            return arr;  
-    }
+        return check === false;
+        };
+    
+/*    for (let value of removeArray){
+        checkValues(editArray, value);
+        filterArray(editArray, value);
+    }*/
 
 }; 
-
 
 console.log(removeFromArray([1,2,3,4], 3));
 console.log(removeFromArray([1,2,3,4], 3, 2));
