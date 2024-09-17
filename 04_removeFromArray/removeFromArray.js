c6873d09977037a0e161e888ba7496bd85e664b7
bcabrera1494 <b.cabrera1494@gmail.com>
2024-09-17 16:46:39 -0700

    Write conditional statements for potential results
    Write conditional logic to sepcify how to proceed
    if remove is NOT present in editArray.
    
    Write conditional logic to specify how to proceed
    if function has not yet accessed all removeFromArray
    elements.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -13,26 +13,7 @@ const removeFromArray = function(arr, ...removeValues) {
     for (let i = 0; i <= removeArray.length; i++){
         let remove = getValue(removeArray, i);
         remove;
-        let newArray = removeItems(editArray, remove);
-        newArray;
-
-        switch (newArray) {
-        
-            case 1:
-                //removeArray contains values that are all present in editArray
-                //remove all values from editArray & return a new array 
-                newArray;
-
-            case 2:
-                //removeArray contains values not present in editArray
-                //ignore and return original array
-
-            case 3: 
-                //removeArray contains a combination of num/str that are present/not present in editArray
-                //ignore non-present values
-                //remove present values 
-                //return a new array
-        }
+        return removeItems (editArray, remove);
     }; 
 };
 console.log(removeFromArray([1,2,3,4], 3));
