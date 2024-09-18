b6223b05b33029666bf917c51148df1e789726a7
bcabrera1494 <b.cabrera1494@gmail.com>
2024-09-18 12:39:16 -0700

    Write pseudocode and initial logic for sum loop

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(startValue, endValue) {
+//Pseudocode
+//Declare variable x = the number that is added to startValue
+//x = startValue + i
+//Use for loop to increment i
+//for (let i = 0; i + x < endValue; i++)
+//{start + (start + x)} return sum
 
+let x = (startValue + 1);
+for (let i = 1; (i + x) < endValue; ){
+    return (start + (start +x));
+    };
 };
 
 // Do not edit below this line
