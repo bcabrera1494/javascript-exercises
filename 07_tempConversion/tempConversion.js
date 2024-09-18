cfb3079ea8b566e160ab19953887d71d87771d39
bcabrera1494 <b.cabrera1494@gmail.com>
2024-09-18 15:48:38 -0700

    Enable all tests.
    All tests now pass.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -15,6 +15,9 @@ const convertToFahrenheit = function(temperature) {
 console.log(convertToCelsius(32));
 console.log(convertToCelsius(100));
 console.log(convertToCelsius(-100));
+console.log(convertToFahrenheit(0));
+console.log(convertToFahrenheit(73.2));
+console.log(convertToFahrenheit(-10));
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
