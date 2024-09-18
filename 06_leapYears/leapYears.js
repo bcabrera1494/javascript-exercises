c76f233cb7afbd466c70d7dd96e89c2ece2c3f57
bcabrera1494 <b.cabrera1494@gmail.com>
2024-09-18 15:07:16 -0700

    Write pesudocode and syntax conditionals
    Use rules defined in README.md to create conditionals
    statements in javascript syntax to return "true" for
    leap years and "false" for non-leap years by checking
    for divisibility by 4, 100, and 400.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,22 @@
-const leapYears = function() {
+const leapYears = function(year) {
+//if the year is divisible by 4 return true (can declare a variable that checks divisibility)
+//if the year is divisible by 100 and NOT divisible by 400 return false
+//if the year is divisible by 100 and divisible by 400 return true can declare a variable that checks divisibilty by 400 
+
+let checkFour = year % 4;
+let checkFourHundred = year % 400;
+let checkOneHundred = year % 100;
+
+if(checkFour === 0){
+    return true;
+};
+
+if(checkOneHundred === 0
+    && checkFourHundred === 0){
+    return true
+ }
+
+else {return false};
 
 };
 
