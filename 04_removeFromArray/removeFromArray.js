643954209cbcab02e4ea2c5fabe6168c71975250
bcabrera1494 <b.cabrera1494@gmail.com>
2024-09-17 18:20:41 -0700

    Removed conditionals for values not in editArray
    Bug causing undefined returns was missing () in
    console.log syntax.
    
    Conditional statements for remove values not present in
    editArray are therefore redundant.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -2,45 +2,38 @@ const removeFromArray = function(arr, ...removeValues) {
     const editArray = Array.from(arr);
     const removeArray = Array.from(removeValues);
 
-    function removeItems (array, valuetoRemove){
-        return array.filter(item => item !== valuetoRemove);
-    };
+
+    function removeItems (array){
+        return array.filter(item => removeArray.includes(item) === false);
+    }
+
     function getValue (array, index) {
         let value = array[index];
         return value;
     };
 
-    for (let i = 0; i <= removeArray.length; i++){
+    for (let i = 0; i < removeArray.length; i++){
+
         let remove = getValue(removeArray, i);
         remove;
-        if (remove === undefined){
-            continue;
-        };
-        //declare variable for edit of array
+
         const newArray = removeItems(editArray, remove);
-        //write conditional logic to sepcify how to proceed if remove is NOT present in editArray
-        if (editArray.includes(remove) === false){
-            continue;
-        };
-        //write conditional logic to specify how to proceed if function has not yet accessed all 
-        //removeArray elements
+
         if ((i + 1) < removeArray.length){
             remove;
-            //declare variable for edit of array
-            let newArrayOne = removeItems (editArray, remove);
-            newArrayOne;
-        }
-
-        return removeItems (editArray, remove);
+            removeItems(editArray, remove);
+            continue;
+        };
+        return newArray;
     }; 
 };
 console.log(removeFromArray([1,2,3,4], 3));
 console.log(removeFromArray([1,2,3,4], 3, 2));
 console.log(removeFromArray([1,2,2,3], 2));
 console.log(removeFromArray([1,2,3,4], 7, "tacos"));
-console.log(removeFromArray[1,2,3,4], 7, 2);
+console.log(removeFromArray([1,2,3,4], 7, 2));
 console.log(removeFromArray([1,2,3,4], 1, 2, 3, 4));
 console.log(removeFromArray(["hey", 2, 3, "ho"], 2, "ho"));
-console.log(removeFromArray([1,2,3]), "1", 3);
+console.log(removeFromArray([1,2,3], "1", 3));
 // Do not edit below this line
 module.exports = removeFromArray;
