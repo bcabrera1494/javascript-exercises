2bf55ef25a3f78596a9e47108d67f6bb37f9d85d
bcabrera1494 <b.cabrera1494@gmail.com>
2024-09-19 09:34:03 -0700

    Console log & enable test 4 subtract

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -6,8 +6,8 @@ const subtract = function(a, b) {
 	return (a-b);
 };
 
-const sum = function() {
-	
+const sum = function(a, ...b) {
+
 };
 
 const multiply = function(a, b) {
@@ -25,6 +25,7 @@ const factorial = function() {
 console.log(add(0,0));
 console.log(add(2,2));
 console.log(add(2,6));
+console.log(subtract(10,4));
 // Do not edit below this line
 module.exports = {
   add,
