99119613ac2220b88c31a7fc5e4ecdd0100633a6
bcabrera1494 <b.cabrera1494@gmail.com>
2024-10-03 16:55:24 -0700

    Enable remaining sum tests. Only sum of empty array currently passes.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -28,6 +28,10 @@ console.log(add(0,0));
 console.log(add(2,2));
 console.log(add(2,6));
 console.log(subtract(10,4));
+console.log(sum([]));
+console.log(sum[7]);
+console.log(sum[7, 11]);
+console.log(sum[1, 3, 5, 7, 9]);
 // Do not edit below this line
 module.exports = {
   add,
