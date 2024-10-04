457be5d4f197fa0930f72a8302912570c3797c9f
bcabrera1494 <b.cabrera1494@gmail.com>
2024-10-04 15:49:08 -0700

    Initialize array fibo [0,1], initialize fLength variable using fibo.length, declare nextValue, which uses reduce method to calculate the next value in the fib sequence. Nest nextValue within a while loop to iterate while fLength < 30. Works as expected for the first 4 values of the sequence, but the 5th value returns 4 instead of 3.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function (index) {
+    // create an array using fibonnacci
+    const fibo = [0, 1];
+    let fLength = fibo.length;
+    // nest in a loop to continue until at least 25
+    while (fLength < 30) {
+        let nextValue = fibo.reduce((last, current) => {
+            return current += last;
+        }, 0);
+        fibo.push(nextValue);
+    };
 
+    // return index-1 of the parameter passed thru the function
+    return fibo[index-1];
 };
-
+console.log(fibonacci(4));
 // Do not edit below this line
 module.exports = fibonacci;
