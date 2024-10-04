5eb3160072426a9a75b41652e6330136c63f6ea4
bcabrera1494 <b.cabrera1494@gmail.com>
2024-10-04 14:18:34 -0700

    Write functions to edit original string and edit backwards string to prepare for comparison. Call the functions and house in variables to be compared at the end of f palindromes() function. Still missing logic to remove spaces between final string characters.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,32 +1,45 @@
 const palindromes = function (string) {
-//PSEUDOCODE
-//Extract characters from string into an array
-const origArray = string.split("");
-//Remove punctuation
-const puncRm = origArray.filter(str => !str.match(/[^\w\s]/g));
-//Reverse the array
-const backwards = puncRm.reverse();
-//Remove the , between each letter
-const fixPal = backwards.join("");
-//Create a new string with the reversed array
-const pal = fixPal.toString();
-//Convert string to lowercase
-const lCase = pal.toLowerCase();
-//Remove punctuation from original string
-const origArrFilter = origArray.filter(str => !str.match(/[^\w\s]/g));
-//Join original array to string
-const origStr = origArrFilter.join("");
-//Convert original string to lowercase
-const origLCase = origStr.toLowerCase()
-//Compare the new string with the original string for equality
-if (origLCase === lCase) {
-    return true;
-}
-else {return false};
+    //PSEUDOCODE
+    //Extract characters from string into an array
+    const origArray = string.split("");
+
+    //Function to edit original string
+    function editOriginal(a) {
+        //Remove punctuation from original string
+        const array = a.filter(str => !str.match(/[^\w\s]/g));
+        //Join original array to string
+        const origStr = array.join("");
+        //Convert original string to lowercase
+        const origLCase = origStr.toLowerCase();
+        return origLCase;
+        //Compare the new string with the original string for equality
+    };
+    //Function to edit backwards string
+    function editBackwards(b) {
+        //Remove punctuation
+        const puncRm = b.filter(str => !str.match(/[^\w\s]/g));
+        //Reverse the array
+        const backwards = puncRm.reverse();
+        //Remove the , between each letter
+        const fixPal = backwards.join("");
+        //Create a new string with the reversed array
+        const pal = fixPal.toString();
+        //Convert string to lowercase
+        const lCase = pal.toLowerCase();
+        return lCase;
+    };
+
+    let original = editOriginal(origArray);
+    let palCheck = editBackwards(origArray);
+    if (original === palCheck) {
+        return true;
+    }
+    else { return false };
 };
 
 console.log(palindromes('racecar'));
 console.log(palindromes('racecar!'));
 console.log(palindromes('Racecar!'));
+console.log(palindromes('A car, a man, a maraca.'));
 // Do not edit below this line
 module.exports = palindromes;
