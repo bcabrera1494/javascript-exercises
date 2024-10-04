1d50b34ef093772a283605e1ab634603ecaf640d
bcabrera1494 <b.cabrera1494@gmail.com>
2024-10-04 14:04:37 -0700

    Enable tests with punctuation and capital letters. Write logic to convert original string to lowercase and to filter punctuation out of both strings. Works as expected.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,19 +1,32 @@
 const palindromes = function (string) {
 //PSEUDOCODE
 //Extract characters from string into an array
-const array = string.split("");
+const origArray = string.split("");
+//Remove punctuation
+const puncRm = origArray.filter(str => !str.match(/[^\w\s]/g));
 //Reverse the array
-const backwards = array.reverse();
+const backwards = puncRm.reverse();
 //Remove the , between each letter
 const fixPal = backwards.join("");
 //Create a new string with the reversed array
 const pal = fixPal.toString();
+//Convert string to lowercase
+const lCase = pal.toLowerCase();
+//Remove punctuation from original string
+const origArrFilter = origArray.filter(str => !str.match(/[^\w\s]/g));
+//Join original array to string
+const origStr = origArrFilter.join("");
+//Convert original string to lowercase
+const origLCase = origStr.toLowerCase()
 //Compare the new string with the original string for equality
-if (string === pal) {
+if (origLCase === lCase) {
     return true;
 }
+else {return false};
 };
 
 console.log(palindromes('racecar'));
+console.log(palindromes('racecar!'));
+console.log(palindromes('Racecar!'));
 // Do not edit below this line
 module.exports = palindromes;
