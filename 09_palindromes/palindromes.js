5bfd39b4d2f02af1fb199f33f1ef81c723b9437c
bcabrera1494 <b.cabrera1494@gmail.com>
2024-10-03 17:52:30 -0700

    Write pseudocode for logic to check if single word is a palindrome.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
 const palindromes = function () {
-
+//PSEUDOCODE
+//Extract characters from string into an array
+//Reverse the array
+//Create a new string with the reversed array
+//Compare the new string with the original string for equality
 };
 
 console.log(palindromes('racecar'));
