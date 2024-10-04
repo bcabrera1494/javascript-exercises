8c7a5dc3576aad4c190aa3c2ab0688c1ae0e6b2e
bcabrera1494 <b.cabrera1494@gmail.com>
2024-10-04 14:25:20 -0700

    Enable all remaining tests. All tests pass. Exercise complete.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -41,5 +41,9 @@ console.log(palindromes('racecar'));
 console.log(palindromes('racecar!'));
 console.log(palindromes('Racecar!'));
 console.log(palindromes('A car, a man, a maraca.'));
+console.log(palindromes('Animal loots foliated detail of stool lamina.'));
+console.log(palindromes('ZZZZ car, a man, a maracaz.'));
+console.log(palindromes('rac3e3car'));
+console.log(palindromes('r3ace3car'));
 // Do not edit below this line
 module.exports = palindromes;
