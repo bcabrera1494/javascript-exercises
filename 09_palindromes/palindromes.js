5c6219e88e85539cafbaec28c3b2fc47d3d65e5a
bcabrera1494 <b.cabrera1494@gmail.com>
2024-10-04 13:43:19 -0700

    Write logic to create an array from string, reverse the array, rejoin the array elements to a string. Works as expected.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,9 +1,17 @@
-const palindromes = function () {
+const palindromes = function (string) {
 //PSEUDOCODE
 //Extract characters from string into an array
+const array = string.split("");
 //Reverse the array
+const backwards = array.reverse();
+//Remove the , between each letter
+const fixPal = backwards.join("");
 //Create a new string with the reversed array
+const pal = fixPal.toString();
 //Compare the new string with the original string for equality
+if (string === fixPal) {
+    return true;
+}
 };
 
 console.log(palindromes('racecar'));
