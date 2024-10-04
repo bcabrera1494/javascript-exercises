1da0d748f6088f8ff3bb9dbc6b59a5e724893897
bcabrera1494 <b.cabrera1494@gmail.com>
2024-10-03 17:04:29 -0700

    Edit sum function to use parameter array instead of a, ...b.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -6,9 +6,9 @@ const subtract = function(a, b) {
 	return (a-b);
 };
 
-const sum = function(a, ...b) {
+const sum = function(array) {
   // use .reduce method to get a sum
-  let result = b.reduce((sum, current) => sum + current, 0);
+  let result = array.reduce((sum, current) => sum + current, 0);
   return result;
 };
 
@@ -29,9 +29,9 @@ console.log(add(2,2));
 console.log(add(2,6));
 console.log(subtract(10,4));
 console.log(sum([]));
-console.log(sum[7]);
-console.log(sum[7, 11]);
-console.log(sum[1, 3, 5, 7, 9]);
+console.log(sum([7]));
+console.log(sum([7, 11]));
+console.log(sum([1, 3, 5, 7, 9]));
 // Do not edit below this line
 module.exports = {
   add,
