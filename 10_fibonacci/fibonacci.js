6b108450a01ff5acac76587954d2f32c12bb70f4
bcabrera1494 <b.cabrera1494@gmail.com>
2024-10-06 15:54:29 -0700

    Added conditionals for returns in case of n == negative number or 0. All tests now pass.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -7,20 +7,29 @@ const fibonacci = function (n) {
                 fibo.push(1);
                 continue;
             } else {
-                let f1 = fibo[i-1];
-                let f2 = fibo[i-2];
+                let f1 = fibo[i - 1];
+                let f2 = fibo[i - 2];
                 let next = f1 + f2;
                 fibo.push(next);
                 continue;
             };
         };
     };
-    return fibo[n-1];
+    if (n == 0) {
+        fibo.unshift(0);
+        return fibo[0];
+    }
+    if (n < 0) {
+        return 'OOPS'
+    } else {
+        return fibo[n - 1]
+    };
 };
 console.log(fibonacci(4));
 console.log(fibonacci(6));
 console.log(fibonacci(10));
 console.log(fibonacci(15));
+console.log(fibonacci(25));
 console.log(fibonacci(0));
 console.log(fibonacci(-25));
 console.log(fibonacci('0'));
