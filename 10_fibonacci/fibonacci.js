3a106bb05eaae3e201f75a1eb190082aa9c3ae90
bcabrera1494 <b.cabrera1494@gmail.com>
2024-10-06 15:30:25 -0700

    Refactor code to use for loop instead of reduce method to create the array of fibonacci sequence. Tests 1-4 now pass.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,18 +1,31 @@
-const fibonacci = function (index) {
-    // create an array using fibonnacci
-    const fibo = [0, 1];
-    // use reduce to append next values to sequence
-    fibo.reduce((prev, current) => {
-    let next = current + prev;
-    return fibo.push(next);
-    }, 1); // initialize at 1 because the formula only works if index > 1
-    console.log(fibo);
-    // return the nth value based on index 
-    return fibo[index];
+const fibonacci = function (n) {
+    const fibo = [];
+    // fibo[n] = fibo[n - 1] + fibo[n - 2];
+    while (fibo.length < n) {
+        for (let i = 0; i < n; i++) {
+            if (i === 0 || i === 1) {
+                fibo.push(1);
+                continue;
+            } else {
+                let f1 = fibo[i-1];
+                let f2 = fibo[i-2];
+                let next = f1 + f2;
+                fibo.push(next);
+                continue;
+            };
+        };
+    };
+    return fibo[n-1];
 };
 console.log(fibonacci(4));
 console.log(fibonacci(6));
 console.log(fibonacci(10));
 console.log(fibonacci(15));
+console.log(fibonacci(0));
+console.log(fibonacci(-25));
+console.log(fibonacci('0'));
+console.log(fibonacci('1'));
+console.log(fibonacci('2'));
+console.log(fibonacci('8'));
 // Do not edit below this line
 module.exports = fibonacci;
