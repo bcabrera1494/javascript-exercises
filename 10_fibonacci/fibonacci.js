f71fb7785c1096f1523408ea1d71bdb354ab05af
bcabrera1494 <b.cabrera1494@gmail.com>
2024-10-06 10:53:32 -0700

    Rework logic for creating fibo array and returning index. Still inaccurate values returned after 3rd value in fibo sequence.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,18 +1,18 @@
 const fibonacci = function (index) {
     // create an array using fibonnacci
     const fibo = [0, 1];
-    let fLength = fibo.length;
-    // nest in a loop to continue until at least 25
-    while (fLength < 30) {
-        let nextValue = fibo.reduce((last, current) => {
-            return current += last;
-        }, 0);
-        fibo.push(nextValue);
-    };
-
-    // return index-1 of the parameter passed thru the function
-    return fibo[index-1];
+    // use reduce to append next values to sequence
+    fibo.reduce((prev, current) => {
+    let next = current + prev;
+    return fibo.push(next);
+    }, 1); // initialize at 1 because the formula only works if index > 1
+    console.log(fibo);
+    // return the nth value based on index 
+    return fibo[index];
 };
 console.log(fibonacci(4));
+console.log(fibonacci(6));
+console.log(fibonacci(10));
+console.log(fibonacci(15));
 // Do not edit below this line
 module.exports = fibonacci;
